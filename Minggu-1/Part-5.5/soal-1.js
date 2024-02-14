4f61da2d88c2436d1e65fad7aeb35dc75f6ebf83
mooooimo <sangleksono41@gmail.com>
2024-02-14 23:44:06 +0700

    Mengerjakan Part 5.5: Tugas 1 - Concat [SELESAI]

---
+++ Minggu-1/Part-5.5/soal-1.js
@@ -0,0 +1,36 @@
+/*
+========================= TUGAS =========================
+1. Let's Form a Sentence
+
+- Problem:
+- Pada tugas ini kamu diminta untuk melakukan penambahan string menggunakan simbol +.
+- Disediakan variable word. Tambahkan nilai word satu per satu dengan nilai variable lain
+- Untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan
+- Tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
+========================= TUGAS =========================
+*/
+
+// ========================= CODE =========================
+
+let word = "JavaScript";
+let second = "is";
+let third = "awesome";
+let fourth = "and";
+let fifth = "I";
+let sixth = "love";
+let seventh = "it!";
+
+//code here
+
+// ---------------------------------------
+console.log(
+    `${word} ` +
+    `${second} ` +
+    `${third} ` +
+    `${fourth} ` +
+    `${fifth} ` +
+    `${sixth} ` +
+    `${seventh}`
+);
+// ---------------------------------------
+// ========================= CODE =========================
